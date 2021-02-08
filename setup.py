c1ddcf3e165dfbd696ae4688c632b6ad2679880e
Alex Shinkevich <alex.shinkevich@gmail.com>
2021-02-09 00:50:26 +0300

    Updated PyPi requirements

--- setup.py
+++ setup.py
@@ -64,13 +64,11 @@ setup(
     classifiers=[
         'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
